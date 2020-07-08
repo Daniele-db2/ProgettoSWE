e7ade2ad794ec2677d1ac773d5fe6f1ba234b79b
Daniele-db2 <52534915+Daniele-db2@users.noreply.github.com>
2020-07-08 15:37:57 +0200

    Update State.java

--- src/State.java
+++ src/State.java
@@ -1,15 +1,3 @@
-//public abstract class Product{
-//    public int code;
-//    public double cost;
-//    public int consegna;
-//
-//    public void setCost(double cost){this.cost = cost;}
-//
-//    public double getCost(){return cost;}
-//
-//    public void setConsegna(int consegna){this.consegna = consegna;}
-//}
-
 public interface State{
     public void doAction(Context context, int abbonamento, Order o);
 }
