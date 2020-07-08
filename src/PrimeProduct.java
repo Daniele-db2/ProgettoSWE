4a32ee7599987f12c6584c6c0c4a8fe0d17eea5d
Daniele-db2 <52534915+Daniele-db2@users.noreply.github.com>
2020-07-08 15:37:31 +0200

    Update PrimeProduct.java

--- src/PrimeProduct.java
+++ src/PrimeProduct.java
@@ -1,39 +1,3 @@
-//public class PrimeProduct extends Product implements Visitable{
-//    public int code;
-//    public double cost;
-//    public int consegna;
-//    public int sconto;
-//
-//    public PrimeProduct(int code){
-//        this.code = code;
-//    }
-//
-//    public void setCost(double c) {
-//        double ct = 5 + Math.random()*45;
-//        ct = Math.floor(ct * 100.0) / 100.0;
-//        cost = ct - ct*((sconto)/100.0);
-//        cost = Math.floor(cost * 100.0) / 100.0;
-//        System.out.print(ct + "€ ---> " + cost + "€, sconto del " + sconto + "%");
-//    }
-//
-//    public double getCost(){
-//        return cost;
-//    }
-//
-//    public void setSconto(int s){
-//        sconto = 10 + ((int)(Math.random() * 20));
-//    }
-//
-//    public void setConsegna(int cons){
-//        consegna = 1 +((int)(Math.random() * 3));
-//    }
-//
-//    @Override
-//    public double accept(Visitor v) {
-//        return v.visit(this);
-//    }
-//}
-
 public class PrimeProduct implements Visitable,State{
     private int code;
     private double cost;
